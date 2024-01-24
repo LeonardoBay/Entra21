e0de9ef372a5df4a84cf0d83ed4ca058dff00a22
LeonardoBay <143364797+LeonardoBay@users.noreply.github.com>
2024-01-24 23:23:13 +0000

    alterações 2

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -1,33 +1,38 @@
-const cadastroPessoa = document.getElementById('cadastroPessoa')
+const login = document.getElementById('login')
 
-function montarJson() {
-    const montarJson = '{' 
-    +' "nome": ' + JSON.stringify(cadastroPessoa.nome.value) + ', ' 
-    +' "sobrenome": ' + JSON.stringify(cadastroPessoa.sobreNome.value) + ', ' 
-    +  ' "idade": ' + JSON.stringify(cadastroPessoa.idade.value) + ', ' 
-    + ' "comidafavorita": ' + JSON.stringify(cadastroPessoa.comidaFavorita.value) + ', '
-    + ' "possuilactose": ' + JSON.stringify(cadastroPessoa.lactose.value) + '}' 
-}
+// function montarJson() {
+//     const montarJson = '{' 
+//     +' "nome": ' + JSON.stringify(cadastroPessoa.nome.value) + ', ' 
+//     +' "sobrenome": ' + JSON.stringify(cadastroPessoa.sobreNome.value) + ', ' 
+//     +  ' "idade": ' + JSON.stringify(cadastroPessoa.idade.value) + ', ' 
+//     + ' "comidafavorita": ' + JSON.stringify(cadastroPessoa.comidaFavorita.value) + ', '
+//     + ' "possuilactose": ' + JSON.stringify(cadastroPessoa.lactose.value) + '}' 
+// }
 
 let contador = 0
 
 function objetoJson(){
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
     // como criar uma variavel
-    const objeto = {
-        nome: cadastroPessoa.nome.value,
-        sobrenome: cadastroPessoa.sobreNome.value,
+    let objeto = {
+        usuario: login.email.value,
+        senha: login.senha.value,
     }
+
     console.log(objeto)
 
-    localStorage.setItem('pessoa', objeto)
-
-    const objetoJson = JSON.stringify(objeto)
+    let objetoJson = JSON.stringify(objeto)
     contador++
+    console.log('dados:' + objeto)
     localStorage.setItem('pessoa' + contador, objetoJson )
+
+    pegarDados();
 }
 
-function pegarDados() {
-    const pessoa = localStorage.getItem('pessoa')
 
-    console.log(pessoa)
+function pegarDados() {
+    let pessoa = localStorage.getItem('pessoa' + contador);
+    console.log(pessoa);
 }
